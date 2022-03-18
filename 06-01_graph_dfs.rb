aa28afe1052943597b6790322237366056ca4ada
atsushi0919 <hatake0919@yahoo.co.jp>
2022-03-18 15:11:11 +0900

    グラフの深さ優先探索 解答追加

--- 06-01_graph_dfs.rb
+++ 06-01_graph_dfs.rb
@@ -1,69 +1,74 @@
-=begin
-グラフの深さ優先探索 (paizaランク B 相当)
-問題にチャレンジして、ユーザー同士で解答を教え合ったり、コードを公開してみよう！
+# グラフの深さ優先探索 (paizaランク B 相当)
+# https://paiza.jp/works/mondai/bfs_dfs_problems/bfs_dfs_problems__graph_dfs
 
-シェア用URL:
-https://paiza.jp/works/mondai/bfs_dfs_problems/bfs_dfs_problems__graph_dfs
-問題文のURLをコピーする
- 下記の問題をプログラミングしてみよう！
-多重辺・自己ループのない無向グラフを構成する 1 〜 N の番号がつけられた頂点とそれらを結ぶ M 本の辺の情報と、頂点番号 X が与えられます。
-頂点 X から次のルールにしたがって深さ優先探索を行った時に訪れる頂点番号を順に出力してください。
+INPUT1 = <<~"EOS"
+  5 6 1
+  1 2
+  2 3
+  2 4
+  3 5
+  4 5
+  1 5
+EOS
+OUTPUT1 = <<~"EOS"
+  1
+  2
+  3
+  5
+  4
+EOS
 
-・現在の頂点に隣接する頂点のうち、未訪問かつ頂点番号が一番小さい頂点を探索する。
+def solve(input_str)
+  # 入力
+  input_lines = input_str.split("\n")
+  n, m, x = input_lines.shift.split.map(&:to_i)
+  paths = input_lines.map { |l| l.split.map(&:to_i) }
 
-例として、以下のグラフで X = 1 から探索を行った場合、答えとなる出力は
+  # 隣接リスト
+  adjacency_list = Hash.new { [] }
+  paths.each do |a, b|
+    adjacency_list[a] <<= b
+    adjacency_list[b] <<= a
+  end
 
+  # dfs
+  # 探索済みの頂点
+  searched_list = Array.new(n, false)
+  # リストをスタックとして使う
+  stack = [x]
+  # 経路
+  visit_list = []
+  while stack.size > 0
+    # 最小番号の頂点を取り出す
+    vertex = stack.pop
 
-1
-5
-3
-6
-8
-9
+    # 探索済みならスキップ
+    next if searched_list[vertex - 1]
 
+    # 現在の頂点を探索済みにする
+    searched_list[vertex - 1] = true
 
-となります。
+    # 現在の頂点を訪問済みリストに追加する
+    visit_list << vertex
 
+    # 全頂点を訪問したら終了
+    break if searched_list.all?
 
-▼　下記解答欄にコードを記入してみよう
+    # 隣接する頂点を調べる
+    # ソート用に一時保管
+    tmp = []
+    adjacency_list[vertex].each do |next_vertex|
+      # 探索済みならスキップ
+      next if searched_list[next_vertex - 1]
 
-入力される値
-N M X
-a_1 b_1
-...
-a_M b_M
+      # 探索候補に追加
+      tmp << next_vertex
+    end
+    # 同じ層でソートしてから探索候補に追加
+    stack += tmp.sort.reverse if not tmp.empty?
+  end
+  # 訪問リストを返す
+  visit_list.join("\n")
+end
 
-
-・ 1 行目では、頂点の数 N と辺の本数 M, 頂点番号 X が半角スペース区切りで与えられます。
-・ 続く M 行では、M 個の辺の両端の頂点の番号 a_i, b_i (1 ≦ i ≦ M) が与えられます。
-
-入力値最終行の末尾に改行が１つ入ります。
-文字列は標準入力から渡されます。 標準入力からの値取得方法はこちらをご確認ください
-期待する出力
-頂点 X から深さ優先探索を行った時に訪れる頂点の番号を順に改行区切りで出力してください。
-
-条件
-すべてのテストケースにおいて、以下の条件をみたします。
-
-・1 ≦ N ≦ 5,000
-・1 ≦ M ≦ min(N*(N-1)/2, 100,000)
-・1 ≦ X ≦ N
-・1 ≦ a_i, b_i ≦ N (1 ≦ i ≦ M)
-・a_i ≠ b_i (1 ≦ i ≦ M)
-
-入力例1
-5 6 1
-1 2
-2 3
-2 4
-3 5
-4 5
-1 5
-
-出力例1
-1
-2
-3
-5
-4
-=end
+puts solve(STDIN.read)
